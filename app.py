8d77d370206d4e892b8c92272592117cf0ea71f5
LFCTO <ton-email@example.com>
2026-09-04 20:43:43 +0200

    Auto-sync MT5 data

--- app.py
+++ app.py
@@ -607,7 +607,6 @@ if not df_hist.empty:
           plot_bgcolor="#161616",
           paper_bgcolor="#0E1117",
       )
-      fig.update_layout(showlegend=False)
       st.plotly_chart(fig_hm, use_container_width=True)
 
     elif granularity == "Tous les mois de l'année":
